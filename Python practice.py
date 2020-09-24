63f71ac4c399597f86449cb89e7e1f0a9e2ec1bb
josephcolonna <71734060+josephcolonna@users.noreply.github.com>
2020-09-24 17:53:18 -0400

    This is version one of my average program

---
+++ Python practice.py
@@ -0,0 +1,49 @@
+Python 3.8.6 (v3.8.6:db455296be, Sep 23 2020, 13:31:39) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 
+>>> 
+>>> print ("Hello World")
+Hello World
+>>> a="Hello"
+>>> b="World"
+>>> a="Goodbye"
+>>> print(a+b)
+GoodbyeWorld
+>>> a="abc123#$"
+>>> print(a)
+abc123#$
+>>> print("abc")
+abc
+>>> type(a)
+<class 'str'>
+>>> a=1
+>>> type(a)
+<class 'int'>
+>>> b=2
+>>> print(a+b)
+3
+>>> a=2.5
+>>> type(a)
+<class 'float'>
+>>> a=True
+>>> a=False
+>>> type(a)
+<class 'bool'>
+>>> not a
+True
+>>> a=True
+>>> not a
+False
+>>> a=2.0
+>>> type(a)
+<class 'float'>
+>>> a=int(a)
+>>> type(a)
+<class 'int'>
+>>> print(a)
+2
+>>> 
+======== RESTART: /Users/joecolonna/Documents/Python/Python practice.py ========
+Please enter a test score: 
